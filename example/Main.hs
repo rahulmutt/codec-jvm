20e4275726dbcf93778af6019f3613fed6bff83d
Rahul Muttineni <rahulmutt@gmail.com>
2016-10-19 10:48:42 +0530

    Updated example codec-jvm

--- example/Main.hs
+++ example/Main.hs
@@ -2,13 +2,25 @@
 module Main where
 
 import Codec.JVM
+import Codec.JVM.ASM.Code.Instr
+import Codec.JVM.ConstPool
 import Data.Monoid((<>))
 
 import Data.Text (Text)
 import qualified Data.ByteString as BS
 
 main :: IO ()
-main = BS.writeFile "Test.class" $ classFileBS classFile
+main = --BS.writeFile "Test.class" $ classFileBS classFile
+  dumpStackMap $
+      new (obj "A")
+   <> dup (obj "A")
+   <> gload jobject 2
+   <> gload jobject 3
+   <> gload jobject 4
+   <> gload jint 5
+   <> iconst jint 0
+   <> iconst jint 0
+   <> invokespecial (mkMethodRef "A" "<init>" (replicate 3 jobject ++ replicate 3 jint) void)
 
 mainClass :: Text
 mainClass = "Test"
@@ -21,3 +33,10 @@ classFile = mkClassFile java7 [Public, Super] mainClass Nothing [] []
      <> ifeq mempty vreturn
      <> vreturn
   ]
+
+dumpStackMap :: Code -> IO ()
+dumpStackMap (Code consts instr) = do
+  putStrLn "Control Flow:"
+  print cf
+  where cp = mkConstPool consts
+        (_, cf, smt) = runInstrBCS instr cp
